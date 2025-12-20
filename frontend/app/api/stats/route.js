dfd8f49da093bfa0ce640e2d06b0fb647d6746d3
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-20 09:41:45 +0100

    Update route.js

--- frontend/app/api/stats/route.js
+++ frontend/app/api/stats/route.js
@@ -3,7 +3,7 @@ import pool from '@/lib/db';
 
 export async function GET(req) 
   const apiKey= req.heaers.get'x-api-key')
-  if (apiKey !== prcessnv.PI_KEY) {
+  if (apiKey !==prcessnv.PI_KEY) {
     return new Netsponse('Unathorized', { status:401 });
   }
   const tvl = await pool.quer(
