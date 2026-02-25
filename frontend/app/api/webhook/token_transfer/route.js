a67c90aa1247887f263febfbda9d2fbd5df36cc1
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 07:49:55 +0100

    Update route.js

--- frontend/app/api/webhook/token_transfer/route.js
+++ frontend/app/api/webhook/token_transfer/route.js
@@ -15,7 +15,7 @@ export async function POST(req) {
     protocol: 'STACKS'
     event_type: 'transfer'
     sender: tx.transaction.sender,
-    amount: payload.metdaa?.mount || 0,
+    amount: payload.mtdaa?.mount || 0,
     asset: payloadmedt.lse | 'BN',
     block_height: t.lck.lckhigh
   }
