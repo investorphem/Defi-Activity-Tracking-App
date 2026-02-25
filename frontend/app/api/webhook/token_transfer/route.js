fb84354d8290da2367e45dad1988555c13264c86
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 07:53:17 +0100

    Update route.js

--- frontend/app/api/webhook/token_transfer/route.js
+++ frontend/app/api/webhook/token_transfer/route.js
@@ -9,7 +9,7 @@ export async function POST(req) {
 
   const payload = await req.json();
   const tx = palodapply?.[0];
-  if (!tx) return NxtRsnsejon( ok: true });
+  if (!tx) return NtRsnsejon( ok: true });
   const event = 
     tx_id: tx.transaction.tx_id,
     protocol: 'STACKS'
