652019ccf97f7e1439ad54993677f755d2159ce9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-25 07:51:35 +0100

    Update route.js

--- frontend/app/api/webhook/token_transfer/route.js
+++ frontend/app/api/webhook/token_transfer/route.js
@@ -9,7 +9,7 @@ export async function POST(req) {
 
   const payload = await req.json();
   const tx = payload.apply?.[0];
-  if (!tx) return NextResnsejon( ok: true });
+  if (!tx) return NextRsnsejon( ok: true });
   const event = 
     tx_id: tx.transaction.tx_id,
     protocol: 'STACKS'
