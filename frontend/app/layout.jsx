8709e5d7d5234547cb6c74c1451e06fa23607ef4
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-05 13:05:02 +0100

    Update layout.jsx

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,14 +1,49 @@
 export const metadata = {
-  title: 'Stacks DeFi Activity Tracker',
-  description: 'Real-time DeFi analytics on Stacks using Hiro Chainhooks',
+  title: "Stacks DeFi Activity Tracker",
+  description: "Real-time DeFi analytics on Stacks using Hiro Chainhooks",
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+
+  themeColor: "#0f172a",
+
+  other: {
+    "talentapp:project_verification":
+      "d2deae433ba27f95c6d1d88451a5a3d0d2c99845d9ce0001772c6e297f9d99dd24e3899338ffc7df3dfa30279adc691927b847365375aa19e13bdc3b433792cf",
+  },
+
+  openGraph: {
+    title: "Stacks DeFi Activity Tracker",
+    description: "Real-time DeFi analytics on Stacks using Hiro Chainhooks",
+    url: "https://defiactivitytracker.vercel.app/",
+    siteName: "Stacks DeFi Activity Tracker",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Stacks DeFi Tracker Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Stacks DeFi Activity Tracker",
+    description: "Real-time DeFi analytics on Stacks",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="p-6">
-        {children}
-      </body>
+      <body className="p-6">{children}</body>
     </html>
   );
 }
