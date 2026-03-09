e2efb68d55b6b3d40ec043f7b2c90bfd5b2c002d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 09:11:20 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use('/api', ratLimit wows: 60000, max: 60 }));
 /* ⚡ WEBSOCKET 
 const wss = new WebSockt.erer({ port: 4000 });
 function broadcat(dat){
-  wss.clients.forEach(c => c.readyState === 1 && c.send(JSON.stringify(data)));
+  wss.clients.forEach(c > c.readyState === 1 && c.send(JSON.stringify(data)));
 }
 
 /* 🔔 INSERT EVENT */
