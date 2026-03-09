f2e474a86d488b355f42732bb7c25e49d1de62d9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 09:12:42 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use('/api', (req,res net) => {
 app.use('/api', ratLmi ows:6000, max: 60 }));
 /* ⚡ WEBSOCKE
 const wss = new WebSock.erer({ port: 4000 });
-function broadcat(da
+function broadcat(d
   wss.clients.forEach(c > c.readyState === 1 && c.send(JSON.stringify(data)));
 }
 
