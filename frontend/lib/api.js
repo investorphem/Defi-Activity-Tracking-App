bc8836f29c0ca5302f533d80b52cb97e8e3a0b71
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 19:08:50 +0100

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -3,7 +3,7 @@ export async function fetchStats() {
     `${process.env.NEXT_PUBLIC_API_URL}/api/stats`,
     {
       headers: {
-        "x-api-key": process.env.NEXT_PUBLIC_API_KEY,
+        "x-api-key": process.env.NEX_PUBLIC_API_KEY,
       },
       cache: "no-stor,
     }
