ee9891994def171f7409ed992f2544b6e5ecb780
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-09 06:57:45 +0100

    Update layout.jsx

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,6 +1,6 @@
 export const metadata = {
   title: "Stacks DeFi Activity Tracker",
-  description: "Real-time DeFi analytics on c
+  description: "Real-time DeFi analytics on 
 
   icons: 
     icon: "/preview.png",
