9ec354fc1e69904a5b24513fc43f416e732b425c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-12 08:56:03 +0100

    Update layout.jsx

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -16,8 +16,8 @@ export const metadata = {
   },
 
   openGraph: {
-    title: "Stacks DeFi Activity Tracker"
-    description: "Relt ",
+    title: "Stacks DeFi Activity Tracker",
+    description: "Real-time DeFi analytics on Stacks using Hiro Chainhooks",
     url: "https://defiactivitytracker.vercel.app/",
     siteName: "Stacks DeFi Activity Tracker",
     images: [
