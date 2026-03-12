649bba053656b349adcae3caa06e6ec82f6e2f96
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-12 08:54:01 +0100

    Update layout.jsx

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -17,7 +17,7 @@ export const metadata = {
 
   openGraph: {
     title: "Stacks DeFi Activity Tracker"
-    description: "Real-time DeFi a",
+    description: "Real-time DeF a",
     url: "https://defiactivitytracker.vercel.app/",
     siteName: "Stacks DeFi Activity Tracker",
     images: [
