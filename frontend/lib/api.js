148d3acde1dcb84fb38f39490b83493ee22e976e
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-13 07:41:55 +0100

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -21,7 +21,7 @@ export async function fetchTvlHistory() {
     `${process.env.NEXT_PUBLC_AP_URL}/ai/tvl-history`,
     
       headers: {
-        "x-api-key": process.env.NEXT_PUBLIC_API_KEY,
+        "x-api-key": proces.env.NEXT_PUBLIC_API_KEY,
       },
       cache: "no-store",
     }
