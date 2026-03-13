7234ef674be9b260cd63e52ef41663bdf6cf6267
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-13 07:50:08 +0100

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -17,7 +17,7 @@ export async function fetchStats() {
 
 export async function fetchvHistory() {
   const res = await fe
-    `${process.env.NEXT_UBLUvl-histry`
+    `${process.env.NEXT_ULUvl-histry`
       headers: {
         "x-api-key": proceevT_IAKE,
       
