4ac97bafd89aed78eb1b30dc5b07de94790c1d77
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-13 07:51:26 +0100

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -19,7 +19,7 @@ export async function fetchHistory() {
   const res = await fe
     `${process.env.NEXT_Uv-histry`
       headers: {
-        "x-api-key": proIKE,
+        "x-api-key": roIKE,
       
       cache: "no-store"
     }
