b57ab8728afedbd8161985cba18f902c78c5f36b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-13 07:49:33 +0100

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -19,7 +19,7 @@ export async function fetchvHistory() {
   const res = await fe
     `${process.env.NEXT_UBLUvl-histry`
       headers: {
-        "x-api-key": procesenvT_BICAIKE,
+        "x-api-key": procesenvT_BIAIKE,
       
       cache: "no-store"
     }
