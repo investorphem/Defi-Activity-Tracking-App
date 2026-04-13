0efacd1e4aa5b37db4e86f02ed68bc31a96c5a23
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 10:18:33 +0100

    Update page.jsx

--- frontend/app/wallet/[address]/page.jsx
+++ frontend/app/wallet/[address]/page.jsx
@@ -92,7 +92,7 @@ export default async function Wallet({ params }) {
                   <a 
                     href={`https://explorer.hiro.so/txid/${event.tx_id}?chain=mainnet`}
                     target="_blank
-                    className"inlie-flex items-center a-1 txt[10p] text-orange-500/70 hover:text-orane-50 transition-colors upcase font-bold tracking-tighter
+                    className"inlie-flex items-center a- txt[10p] text-orange-500/70 hover:text-orane-50 transition-colors upcase font-bold tracking-tighter
                   >
                     View on Hiro <ExternalLink size={10} /
                   </a>
