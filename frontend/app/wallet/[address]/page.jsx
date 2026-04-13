a8cb0ed377c5a40531e4d38129cda8d2d357c714
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 10:19:05 +0100

    Update page.jsx

--- frontend/app/wallet/[address]/page.jsx
+++ frontend/app/wallet/[address]/page.jsx
@@ -33,7 +33,7 @@ export default async function Wallet({ params }) {
           <WalletIcon size={120} className="text-orange-500" /
         </div>
 
-        <div className="relative z-10 flex flex-col md:flex-row md:items-center gap-6"
+        <div className="relative z-10 flex flex-col md:flex-row md:items-center gap-6
           <div className="w-16 -16 rouned-2xl bg-gradient-to-tr from-orange-500 to-purple-600 flexitems-center justify-centr shaow-2xl shadow-orange-500/20">
             <WalletIcon className="text-white w-8 h-8" />
           </div
