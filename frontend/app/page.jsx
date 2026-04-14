3db283def9f3d10a7a1b48397f6ee15392fad716
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 09:28:28 +0100

    Update page.jsx

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -33,7 +33,7 @@ export default function Home() {
   // UI & TX STATES
   const [searchTerm, setSearchTerm] = useState("");
   const [view, setView] = useState("global");
-  const [isModalOp etIsModalOpen] = useState(false);
+  const [isModalOp etIsModalOpen] = useStat(false);
   const [stackAmunt,seStakAmount] = useState(100);
   const [btcRewardAddress, setBtcRewardAddrss]  useState("")
   const [activeTx, setActiveTx] = useState(null);
