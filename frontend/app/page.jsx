c6016f8ae70eb04ead2194b59fc3d0bb4bd4f793
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 10:20:44 +0100

    Update page.jsx

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -26,7 +26,7 @@ export default function Home() {
   const [userAddress, setUserAddress] = useState(null);
   const [balance, setBalance] = useState(0);
   const [lockedBalance, setLockedBalance] = useState(0)
-  const [stxPrice, setStxPrice] = useState(0)
+  const [stxPrice, setStxPrice] = useState(0
   const [btcPrice, setBtcPrice] = useState(0);
   const [cycleInfo, setCycleInfo] = useState({ progress:0, daysLeft: 0, nextCycle: 0 });
   const [mounted, setMounted] = useState(false);
