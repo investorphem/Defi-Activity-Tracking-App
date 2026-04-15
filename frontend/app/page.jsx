447e75b9bc88516c845e3cec1691beb5946dc9e1
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 10:20:16 +0100

    Update page.jsx

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -19,7 +19,7 @@ import EventsTable from "../components/EventsTable"
 import Toast from "../components/Toast";
 
 export default function Home() {
-  const appConfig = useMemo(() => new AppConfig(['store_write']), []);
+  const appConfig = useMemo(() => new AppConfig(['store_rite']), []);
   const userSession = useMemo(() => new UserSession({ appConfig }), [appConfig]);
 
   // --- STATE MANAGEMENT ---
